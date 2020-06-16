d0b74739a6bdb30bd11e2b6675fdbf2c20a4fd4c
WCFX <wagnerfx.dev@gmail.com>
2020-06-16 18:55:51 -0300

    Layout concluído. Hora de criar as funções do app

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(
     home: Home(),
-    )
-  );
+  ));
 }
+
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
@@ -15,17 +16,63 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Calculadora de IMC'),
-        centerTitle: true,
-        backgroundColor: Colors.teal[300],
-        actions: <Widget>[
-          IconButton(icon: Icon(Icons.refresh),
-          onPressed: () {},
-          )
-        ],
-      ),
-      
-    );
+        appBar: AppBar(
+          title: Text('Calculadora de IMC'),
+          centerTitle: true,
+          backgroundColor: Colors.teal[300],
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: () {},
+            )
+          ],
+        ),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          padding: EdgeInsets.fromLTRB(10.0,0.0, 10.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Icon(Icons.person_pin, size: 120.0, color: Colors.teal[300]),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Peso (KG)",
+                  labelStyle: TextStyle(color: Colors.teal[300]),
+                ),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.teal[300], fontSize: 25.0),
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Altura (CM)",
+                  labelStyle: TextStyle(color: Colors.teal[300]),
+                ),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.teal[300], fontSize: 25.0),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 40.0, bottom: 20.0),
+                child: Container(
+                height: 50.0,
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Calcular",
+                    style: TextStyle(color: Colors.white, fontSize: 25.0),
+                  ),
+                  color: Colors.teal[300],
+                ),
+                )
+              ),
+              Text(
+                "Info",
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.teal[300], fontSize: 25.0),
+              )
+            ],
+          ),
+        ));
   }
 }
